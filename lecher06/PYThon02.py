003eead561a79021cc17cbb4d3f9a49d4abd2cfc
tipawan665241009 <142291445+tipawan665241009@users.noreply.github.com>
2023-09-12 12:58:19 +0700

    Add files via upload

---
+++ lecher06/PYThon02.py
@@ -0,0 +1,7 @@
+# คำสั่ง if
+if 7 < 20 ** 2 :
+    print('ho ho ho')
+    print('Yare Yare')
+    print('muda muda muda')
+
+    print('ora ora ora')
---
+++ lecher06/dtiworkshop02.py
@@ -0,0 +1,13 @@
+#
+#
+#วิเคราะห์
+#มองหา
+
+
+
+def inputCarDetail() :
+    carNo = input('ป้อนทะเบียนรถ : ')
+    carweight = fioat(input('ป้อนน้ำหนักรถ :'))
+    return caeNo,carweight
+
+def checkcarAndsh
