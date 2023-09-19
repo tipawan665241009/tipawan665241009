dc515ed72234deb68e0327d7a79434dabfb68bfc
tipawan665241009 <142291445+tipawan665241009@users.noreply.github.com>
2023-09-19 11:37:42 +0700

    Add files via upload

--- lecher06/dtiworkshop02.py
+++ lecher06/dtiworkshop02.py
@@ -1,13 +1,31 @@
-#
-#
+#โปรมเเกมตรวจสอบน้ำหนักรถบรรทุกของของด่านข้างน้ำหนักว่ารถบรรทุกนั้นมีน้ำหนักรถผ่านเกณฑ์หรือไม่
+#หากน้ำหนักเกิน 1000kg ให้เเสดงข้อความว่า "รถน้ำหนักผ่านเกรณฑ์" เเต่หากน้ำหนักตั้งเเต่
+#1000kg ลงมาให้เเสดงข้อความว่า "รถน้ำหนักผ่านเกรณฑ์" โดยให้ป้อนทะเบียนรถบรรทุก
+#เเละน้ำหนักรถบรรทุกทางเเป้นพิมพ์         
+
 #วิเคราะห์
-#มองหา
+#มองหา feature การทำงานว่ามีอะไรบ้าง เพื่อจะไปสร้างเป็น function
+# รับค่าทะเบียนรถ น้ำหนัก -> inputCarDetail
+# ตรวจสอบน้ำหนักรถ เเละเเสดงผล -> checkCarAndShowWeight
+
+def inputCarDetail():
+    carNo = float(input('ป้อนทะเบียนรถ: '))
+    carweight = float(input("ป้อนน้ำหนักรถ: "))
+    return carNo, carweight
+
+def checkCarAndShowWeight(carNo, carweight):
+    if carweing > 1000:
+        print (f'ทะเบียนรถ {carNo} น้ำหนักไม่ผ่านเกรณฑ์')
+    else:
+        print(f'ทะเบียนรถ {carNo} น้ำหนักผ่านเกรณฑ์')
 
 
 
-def inputCarDetail() :
-    carNo = input('ป้อนทะเบียนรถ : ')
-    carweight = fioat(input('ป้อนน้ำหนักรถ :'))
-    return caeNo,carweight
+print("----------------------")
+print("TruckChecker")
+print("----------------------")
 
-def checkcarAndsh
+CarNo, carweight = inputCarDetail()
+print("-------------------------------")
+checkCarAndShowWeight(carNo, carweight)
+print("-------------------------------")
