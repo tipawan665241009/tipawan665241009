bee1b7def18ff20c44887c0d65e4f007f4dcb1e3
tipawan665241009 <142291445+tipawan665241009@users.noreply.github.com>
2023-10-17 12:18:16 +0700

    Add files via upload

---
+++ saupythonproject/lecher08/test04.py
@@ -0,0 +1,20 @@
+class Test04 :
+    datal = 10
+
+    #constructor
+    def _init_(self,data2,data3)
+        priut('Hi...')
+        self.data2 = data2
+        self.dada3 = data3
+
+    def showSumData(self) :
+        print(self.datal + self.data2 + self.data3)
+        salf.showow()
+
+    def showow(self) :
+        print('Wow Wow Wow...')
+
+objA = Test04(20,30)
+objB = Test04(10,20)
+objC = Test04(20,30)
+
