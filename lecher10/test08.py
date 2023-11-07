179057ac2a1a845ae696851dd808c5c85695e0c1
tipawan665241009 <142291445+tipawan665241009@users.noreply.github.com>
2023-11-07 10:59:18 +0700

    Add files via upload

---
+++ lecher10/test08.py
@@ -0,0 +1,10 @@
+# ลบไฟล์
+import os
+from os import remove
+
+if os. path.exists("myfile01.txt")
+   #os.remove("myfile01.yxt")
+   remove('myfile02.txt')
+   print('ลบไปเรียบร้อยเเล้ว')
+else : 
+    print('ไฟล์ที่จะลบไม่มี')
